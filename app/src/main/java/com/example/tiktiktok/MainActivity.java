55c1c0c4d9083fc4337294c3c0d5976d1f799618
Andrina Geller <andrinage01@gmail.com>
2023-04-25 14:12:02 +0200

    add a main screen and changes to setting screen

---
+++ app/src/main/java/com/example/tiktiktok/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tiktiktok;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.tiktiktok.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    Button goToSettingsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        goToSettingsButton = findViewById(R.id.goToSettingsButton);
+        goToSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, SettingsActivity.class);
+        startActivity(switchActivityIntent);
+    }
+}
